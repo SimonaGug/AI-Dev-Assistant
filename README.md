f6a4028a5ff3b2424c8c5467919ed07ca234cea0
sgut6y <simona.gugliermo@scania.com>
2025-05-13 14:30:04 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # My New Repo
+
+yarn install in backend
+
+yarn add langchain
+
+yarn add -D @types/cors @types/dotenv
+run yarn in /frontend and /backend
+
+Add your OPENAI_API_KEY in backend/.env
---
+++ backend/prisma/migrations/20250512182630_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Query" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "query" TEXT NOT NULL,
+    "response" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
